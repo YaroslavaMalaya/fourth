3bf0ec43bc3385551d42ec5c3f5c0492780d3fc2
Kristina <kristinaer304@gmail.com>
2023-04-06 20:13:10 +0300

    counting the frequency of symbols

--- lab_4/Program.cs
+++ lab_4/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Program
+{   
+    
+    static void Main(string[] args)
+    {
+        
+        using (StreamReader reader = new StreamReader("sherlok.txt"))
+        {
+            
+            Dictionary<char, int> charFreq = new Dictionary<char, int>();
 
-Console.WriteLine("Hello, World!");
+            
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                
+                foreach (char c in line)
+                {
+                    
+                    if (charFreq.ContainsKey(c))
+                    {
+                        
+                        charFreq[c]++;
+                    }
+                    else
+                    {
+                        
+                        charFreq[c] = 1;
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<char, int> pair in charFreq)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+        }
+    }
+    
+}
