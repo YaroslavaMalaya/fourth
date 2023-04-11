e8ecc82be2c98fe8da67ec97c48b7e42849ce30e
Yaroslava Mala <ymala@kse.org.ua>
2023-04-11 11:24:29 +0300

    added Sherlock_byte

--- lab_4/Program.cs
+++ lab_4/Program.cs
@@ -115,7 +115,39 @@ foreach (var character in text3)
     check += c;
     write2.Flush();
 }
+
+FileStream fs4 = new FileStream("Sherlock_byte.txt", FileMode.OpenOrCreate);
+StreamWriter write4 = new StreamWriter(fs4);
+var text4 = File.ReadAllText("Sherlock_code.txt");
+
+var bytes = GetBytes(text4);
+fs4.Write(bytes);
+
 write.Close();
 write2.Close();
+write4.Close();
 fs.Close();
 fs2.Close();
+fs4.Close();
+
+byte[] GetBytes(string line)
+{
+    string[] byteStrings = new string[line.Length / 8];
+    for (int i = 0; i < line.Length-8; i += 8)
+    {
+        byteStrings[i / 8] = line.Substring(i, 8);
+        //Console.WriteLine(byteStrings[i / 8]);
+    }
+    byteStrings[^1] = line.Substring(byteStrings.Length*8);
+    while (byteStrings[^1].Length != 8 )
+    {
+        byteStrings[^1] += "0";
+    }
+    //Console.WriteLine(byteStrings[^1]);
+    byte[] bytes = new byte[byteStrings.Length];
+    for (int i = 0; i < byteStrings.Length; i++)
+    {
+        bytes[i] = Convert.ToByte(byteStrings[i], 2);
+    }
+    return bytes;
+}
